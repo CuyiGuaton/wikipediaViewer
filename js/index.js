435508e5f367c2d0b5241f7597f298dc63d1067c
Sergio Salinas <sergiosalfer@gmail.com>
2016-08-11 19:29:27 -0400

    agregada api wikipedia

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
+fuction searchInWiki( search ){
+  $.ajax({
+    url: 'http://en.wikipedia.org/w/api.php',
+    data: { action: 'query', list: 'search', srsearch: 'pico', format: 'json' },
+    dataType: 'jsonp',
+    success: function (x) {
+      console.log('title', x.query.search[2].title);
+    }
+  });
+}
+
 $( document ).ready(function() {
-    console.log( "ready!" );
+  $( "#search" ).click(searchInWiki
+  });
+
 });
