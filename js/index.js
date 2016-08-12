2921ff1ed8731de21f91ab0747d6c89bc8ee22d9
Sergio Salinas <sergiosalfer@gmail.com>
2016-08-11 23:57:24 -0400

    Terminado el get de la api de wikipedia, solo falta usar los datos

--- js/index.js
+++ js/index.js
@@ -1,44 +1,44 @@
-function searchInWiki() {
-    //https://en.wikipedia.org/w/api.php?action=opensearch&search=bee&limit=1&format=json
+function searchInWiki(search) {
+    //http://stackoverflow.com/questions/8930867/wikipedia-list-search-rest-api-how-to-retrieve-also-url-of-matching-articles
+    //https://en.wikipedia.org/w/api.php?action=opensearch&search=pico&aplimit=10&&format=jsonfm
+    //El ajax de abajo lo conseguiste de
+    //http://stackoverflow.com/questions/25891076/wikipedia-api-fulltext-search-to-return-articles-with-title-snippet-and-image
+    //https://en.wikipedia.org/w/api.php?format=jsonfm&action=query&generator=search&gsrnamespace=0&gsrsearch=test&gsrlimit=10&prop=pageimages|extracts|info&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&inprop=url
+    //son lo casi mismo (cambiar jsonfm por json y quitar callback para ver, callback se pone al final del link para enviar algo a la wiki y no te de error)
+    //$.getJSON("https://en.wikipedia.org/w/api.php?format=jsonfm&action=query&generator=search&gsrnamespace=0&gsrsearch=test&gsrlimit=10&prop=pageimages|extracts|info&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&inprop=url&callback=?", function (data){
     $.ajax({
-      url: "https://en.wikipedia.org/w/api.php",
-      data: {
-        "action": "query",
-        "format": "json",
-        "generator": "search",
-        "gsrlimit": 10,
-        "prop": "info|pageimages|extracts",
-        "exintro": 1,
-        "explaintext": 1,
-        "exsentences": 1,
-        "exlimit": "max",
-        "pilimit": "max",
-        "inprop": "url",
-        "redirects": 1,
-        "gsrsearch": "pico",
-      },
-      dataType: 'jsonp',
-      type: 'POST',
-      async: false,
-      headers: {
-        'Api-User-Agent': 'Example/1.0'
-      },
-      success: function(data) {
-        var pages = data.query.pages;
-        $("#result").html("data");
-      },
-      error: function(err) {
-        alert(err);
-      }
+        url: 'http://en.wikipedia.org/w/api.php',
+        data: {
+          "format": "json",
+          "action":"query",
+          "generator":"search",
+          "gsrnamespace":0,
+          "gsrsearch":search,
+          "gsrlimit":10,
+          "prop":"pageimages|extracts|info",
+          "exintro":1,
+          "explaintext":1, // Muestra en texto plano en vez de html
+          "pilimit":"max",
+          "exsentences":1,
+          "exlimit":"max",
+          "inprop":"url" //muestra la url canonica, pero necesita prop= info para verla
+        },
+        dataType: 'jsonp',
+        type: 'POST',
+        async: false,
+        headers: {
+            'Api-User-Agent': 'Example/1.0'
+        },
+        success: function(data) {
+            $("#result").html("data");
+        },
+        error: function(err) {
+          alert(err);
+        }
     });
 }
 
-// $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=pico&limit=10&namespace=0&format=json&callback=?", function (data){
-//   $("#result").html(data[1][1]);
-//
-// });
 
-
-$("form").submit(function(e) {
-    searchInWiki();
+$("form").submit(function() {
+    searchInWiki( $("#search").val());
 });
