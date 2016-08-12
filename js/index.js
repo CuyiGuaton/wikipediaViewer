245b829d3674371ac27e209b3b4df94cb1e452ef
Sergio Salinas <sergiosalfer@gmail.com>
2016-08-11 20:35:20 -0400

    Se obtiene el valor del submit, el boton enviar no es necesario

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
-fuction searchInWiki( search ){
+function searchInWiki( search ){
   $.ajax({
     url: 'http://en.wikipedia.org/w/api.php',
     data: { action: 'query', list: 'search', srsearch: 'pico', format: 'json' },
     dataType: 'jsonp',
-    success: function (x) {
+    success: function (data) {
       console.log('title', x.query.search[2].title);
     }
   });
 }
 
-$( document ).ready(function() {
-  $( "#search" ).click(searchInWiki
-  });
-
+$( "form" ).submit(function() {
+alert($("#search").val());
 });
